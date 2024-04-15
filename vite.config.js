92115b89ddf755ce51245bc6f83b9c73ded72d7f
Gitit2meBaby <thisworldofdans@gmail.com>
2024-04-15 18:28:49 +0700

    changed config file for all page entry points

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: './src',
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'src/index.html', // Adjusted path for index.html
+                byd: 'src/html/BYD.html', // Adjusted path for BYD.html
+                sma: 'src/html/SMA.html', // Adjusted path for SMA.html
+                downloads: 'src/html/downloads.html', // Adjusted path for downloads.html
+                reviews: 'src/html/reviews.html', // Adjusted path for reviews.html
+                blog: 'src/index.html', // Adjusted path for blog.html
+                winaico: 'src/html/winaico.html', // Adjusted path for winaico.html
+                radiant: 'src/html/radiant.html', // Adjusted path for radiant.html
+                privacy: 'src/html/privacyPolicy.html', // Adjusted path for privacyPolicy.html
+                faqs: 'src/html/faqs.html', // Adjusted path for faqs.html
+            },
+        },
+    },
     optimizeDeps: {
         include: ['*.html'],
     },
