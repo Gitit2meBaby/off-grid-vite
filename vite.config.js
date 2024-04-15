3b27254f468c53b0114b470ae0434c03d5c96fe8
Gitit2meBaby <thisworldofdans@gmail.com>
2024-04-15 18:58:39 +0700

    brought all html files back to root, updated paths over entire project

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: 'src/index.html', // Adjusted path for index.html
-                byd: 'src/html/BYD.html', // Adjusted path for BYD.html
-                sma: 'src/html/SMA.html', // Adjusted path for SMA.html
-                downloads: 'src/html/downloads.html', // Adjusted path for downloads.html
-                reviews: 'src/html/reviews.html', // Adjusted path for reviews.html
+                byd: 'src/BYD.html', // Adjusted path for BYD.html
+                sma: 'src/SMA.html', // Adjusted path for SMA.html
+                downloads: 'src/downloads.html', // Adjusted path for downloads.html
+                reviews: 'src/reviews.html', // Adjusted path for reviews.html
                 blog: 'src/index.html', // Adjusted path for blog.html
-                winaico: 'src/html/winaico.html', // Adjusted path for winaico.html
-                radiant: 'src/html/radiant.html', // Adjusted path for radiant.html
-                privacy: 'src/html/privacyPolicy.html', // Adjusted path for privacyPolicy.html
-                faqs: 'src/html/faqs.html', // Adjusted path for faqs.html
+                winaico: 'src/winaico.html', // Adjusted path for winaico.html
+                radiant: 'src/radiant.html', // Adjusted path for radiant.html
+                privacy: 'src/privacyPolicy.html', // Adjusted path for privacyPolicy.html
+                faqs: 'src/faqs.html', // Adjusted path for faqs.html
             },
         },
     },
